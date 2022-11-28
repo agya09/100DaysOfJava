0d612ed2c640d81f9ee5744fb0206614be929cef
agya09 <pathakagya07@gmail.com>
2022-11-28 22:10:58 +0545

    word count

---
+++ 17th Day/README.md
@@ -0,0 +1,1 @@
+program to count words in string
---
+++ 17th Day/wordCount.java
@@ -0,0 +1,10 @@
+public class wordCount {
+
+    public static void main(String[] args) {
+        String message = "lLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.        ";
+        int countWord = message.split("\\s").length;
+        System.out.println("The total words  in above string is : " + countWord);
+
+    }
+
+}
