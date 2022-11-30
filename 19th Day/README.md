5e75d1b8f38d2d9d3d9d6c696c468409b0c1e534
agya09 <pathakagya07@gmail.com>
2022-11-30 22:59:09 +0545

    Character replace

---
+++ 19th Day/README.md
@@ -0,0 +1,1 @@
+program to replace characters in string
---
+++ 19th Day/charReplace.java
@@ -0,0 +1,7 @@
+public class charReplace {
+    public static void main(String[] args) {
+        String mystr = "Hello World";
+        String newstr = mystr.replace('l', 'p');
+        System.out.println(newstr);
+    }
+}
