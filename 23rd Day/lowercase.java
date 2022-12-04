f2b741997990c86f3d21c97b517b5fdedd1e4972
agya09 <pathakagya07@gmail.com>
2022-12-04 21:34:50 +0545

    lowercase

---
+++ 23rd Day/lowercase.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+
+public class lowercase {
+    public static void main(String[] args) {
+        String mystr = "JAVA PRograMMinG LanguagE";
+        String newstr = mystr.toLowerCase(Locale.ENGLISH);
+        System.out.println(newstr);
+
+    }
+}
