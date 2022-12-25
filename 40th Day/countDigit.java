71698845cfb5fdb78357c0d6f85aec65c9ea2884
agya09 <pathakagya07@gmail.com>
2022-12-25 21:41:08 +0545

    Digit Count

---
+++ 40th Day/countDigit.java
@@ -0,0 +1,20 @@
+public class countDigit {
+    public static void main(String[] args) {
+        System.out.println(isCountDigit(23333331, 3));
+    }
+
+    public static int isCountDigit(int n, int digit) {
+        if (n < 0 || digit < 0)
+            return -1;
+        int count = 0;
+        while (n > 0) {
+            if (n % 10 == digit)
+                count++;
+            n = n / 10;
+        }
+
+        return count;
+
+    }
+
+}
