a9b28f6246edf25e7fc946e4a46523adcac998fd
agya09 <pathakagya07@gmail.com>
2023-02-07 18:50:10 +0545

    Duck number

---
+++ 72th Day/README.md
@@ -0,0 +1,3 @@
+A java program to check whether the given number is a duck number or not. 
+Duck number :
+Duck number is a special positive non-zero number that contains zero in it and zero should not be present at the start of the number.
---
+++ 72th Day/duckNum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class duckNum {
+    public static boolean isDuck(int number){
+        while(number != 0){
+            if(number%10 ==0)
+            return true;
+
+            number = number/10;
+        }
+        return false;
+
+
+    }
+    public static void main(String[] args) {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        n = s.nextInt();
+
+        if(isDuck(n))
+        System.out.println(n + " is a duck number");
+        else
+        System.out.println(n + " is not a duck number");
+    }
+    
+}
